8229002eba6ede7968e9609cc69d44da03a5b30d
Louis Painter <louis.painter49@gmail.com>
2026-02-13 00:17:41 +0100

    fix(autorotate): make script executable

--- home/dotfiles/autorotate.sh
+++ home/dotfiles/autorotate.sh
@@ -6,12 +6,8 @@ DEVICE="/dev/input/event8"
 stdbuf -oL libinput debug-events --device "$DEVICE" | while IFS= read -r line; do
 
     if echo "$line" | grep -q "switch tablet-mode state 1"; then
-        gnome-randr modify "$OUTPUT" -r inverted 2>&1 | while IFS= read -r err; do
-            echo "[autorotate][ERROR] $err"
-        done
+        gnome-randr modify "$OUTPUT" -r inverted 2>&1
     elif echo "$line" | grep -q "switch tablet-mode state 0"; then
-        gnome-randr modify "$OUTPUT" -r normal 2>&1 | while IFS= read -r err; do
-            echo "[autorotate][ERROR] $err"
-        done
+        gnome-randr modify "$OUTPUT" -r normal 2>&1
     fi
 done
--- home/modules/dotfiles.nix
+++ home/modules/dotfiles.nix
@@ -1,6 +1,10 @@
 {
   home.file = {
     ".config/starship.toml".source = ../dotfiles/starship.toml;
-    "bin/autorotate.sh".source = ../dotfiles/autorotate.sh;
+
+    "bin/autorotate.sh" = {
+      source = ../dotfiles/autorotate.sh;
+      executable = true;
+    };
   };
 }
