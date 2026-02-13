fd25612cedfd41d83eafe59c88ddb38ed337fcda
Louis Painter <louis.painter49@gmail.com>
2026-02-13 11:08:15 +0100

    wip(battery): cap cpu freq on battery

--- hosts/laptop/power-usage.nix
+++ hosts/laptop/power-usage.nix
@@ -5,6 +5,7 @@
   services.power-profiles-daemon.enable = false;
 
   powerManagement.powertop.enable = true;
+  services.thermald.enable = true;
 
   programs.auto-cpufreq.enable = true;
   programs.auto-cpufreq.settings = {
@@ -12,10 +13,16 @@
       ideapad_laptop_conservation_mode = true;
       governor = "powersave";
       turbo = "never";
+      energy_performance_preference = "power";
+      # enable_thresholds = true;
+      # start_threshold = 20;
+      # stop_threshold = 80;
+      scaling_max_freq = 2200000; # 2.2 GHz cap
     };
     charger = {
-      governor = "performance";
-      turbo = "auto";
+      governor = "powersave";
+      turbo = "never";
+      energy_performance_preference = "balance_power";
     };
   };
 }
