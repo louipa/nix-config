07f0bd64b3e0f9eb0eae986ff9d96a2957acfe15
Louis Painter <louis.painter49@gmail.com>
2026-02-16 14:05:02 +0100

    add(cicd): garnix cache

--- configuration.nix
+++ configuration.nix
@@ -4,4 +4,14 @@
     ./system/desktop
     ./system/packages.nix
   ];
+
+  nix.settings = {
+    substituters = [
+      "https://cache.garnix.io"
+    ];
+
+    trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
 }
