b98d0a26b7e2aabf4c80b84cff7b6d5f6b98ef09
Louis Painter <louis.painter49@gmail.com>
2026-02-19 10:59:01 +0100

    add(browsers/firefox): change preferences

--- system/desktop/browsers.nix
+++ system/desktop/browsers.nix
@@ -16,6 +16,30 @@
     policies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
 
       Preferences = {
         "browser.compactmode.show" = true;
@@ -24,10 +48,24 @@
         "browser.tabs.tabmanager.enabled" = false;
         "ui.systemUsesDarkTheme" = 1;
         "browser.toolbars.bookmarks.visibility" = "always";
-        "extensions.formautofill.addresses.enabled" = false;
-        "extensions.formautofill.creditCards.enabled" = false;
-        "signon.rememberSignons" = false;
+        "extensions.formautofill.addresses.enabled" = true;
+        "extensions.formautofill.creditCards.enabled" = true;
+        "signon.rememberSignons" = true;
         "ui.key.menuAccessKeyFocuses" = false;
+        "browser.search.suggest.enabled" = false;
+        "browser.search.suggest.enabled.private" = false;
+        "browser.urlbar.suggest.searches" = false;
+        "browser.urlbar.showSearchSuggestionsFirst" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.translation.neverForLanguages" = "en,fr";
       };
     };
   };
