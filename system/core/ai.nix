946a2fd778eaa43a05b0d55a22c28cf9ca76af61
Louis Painter <louis.painter49@gmail.com>
2026-02-19 21:55:10 +0100

    add(ai): rocm

--- system/core/ai.nix
+++ system/core/ai.nix
@@ -1,11 +1,25 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   services.ollama = {
     enable = true;
+    package = pkgs.ollama-rocm;
+    #package = pkgs.ollama-vulkan
+    #package = pkgs.llama-cpp-rocm;
+    #acceleration = “rocm”;
+
+    environmentVariables = {
+      HIP_VISIBLE_DEVICES = "1";
+      OLLAMA_LLM_LIBRARY = "rocm";
+      HCC_AMDGPU_TARGET = "gfx1152"; # nix run nixpkgs#"rocmPackages.rocminfo" -- --run "rocminfo" | grep "gfx"
+      HSA_ENABLE_SDMA = "1";
+    };
+
     loadModels = [
       "llama3.2:3b"
+      "qwen3:8b"
+      "gemma3:4b"
+      "deepseek-r1:8b"
     ];
-    # syncModels = true;
   };
   services.nixai = {
     enable = true;
