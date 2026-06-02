c3bcbe18b15370a6359f9d81aaa71f5b9bf6df71
Louis Painter <louis.painter49@gmail.com>
2026-06-02 10:07:16 +0200

    chore: garnix shutting down

--- system/common/default.nix
+++ system/common/default.nix
@@ -14,7 +14,5 @@
       "nix-command"
       "flakes"
     ];
-    substituters = [ "https://cache.garnix.io" ];
-    trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
   };
 }
