74be5aede704e4365ddb8d51cbf44f255df508be
Louis Painter <louis.painter49@gmail.com>
2026-07-28 12:35:39 +0200

    chore(ci/substituers): rip garnix my beloved

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -11,8 +11,6 @@
       "nix-command"
       "flakes"
     ];
-    substituters = [ "https://cache.garnix.io" ];
-    trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
   };
 
   nixpkgs.config.allowUnfree = true;
